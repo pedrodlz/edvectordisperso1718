df77ad32174b169796e4edbb3776047363269146
Pedro <pedro.zurgena@hotmail.com>
2017-11-08 19:57:54 +0100

    Añadidos == y !=

--- include/vectorD.hxx
+++ include/vectorD.hxx
@@ -5,7 +5,7 @@
 
 template <class T>
 vectorD<T>::vectorD(const  T  & t){
-  n_ele = 0;                  
+  n_ele = 0;
   v_nulo = t;
 }
 
@@ -46,7 +46,7 @@ typename vectorD<T>::void vectorD<T>::set(int p, const T & t){
 
 	if(t != v_nulo){
 		if(p < n_ele){
-		
+
 			int i = 0;
 			bool sigo = true;
 
@@ -54,7 +54,7 @@ typename vectorD<T>::void vectorD<T>::set(int p, const T & t){
 				if(*it.first == p){
 					*it.first = t;
 					sigo = false;
-				}			
+				}
 				else if(*it.first > p){
 					*it.insert(i,pair<p,t>);
 					sigo = false;
@@ -106,7 +106,7 @@ typename vectorD<T>::vectorD<T> & vectorD<T>::operator=(const vectorD<T> & x)
 	*this.n_ele = x.n_ele;
 	*this.v_nulo = x.v_nulo;
 	*this.vd = x.vd;
-	
+
 	return this;
 }
 
@@ -115,16 +115,39 @@ typename vectorD<T>::const T & vectorD<T>::operator[](int c) const
 {
 	bool sigo = true;
 	T* referencia;
-		
+
 	for(std::list<T>::iterator it = vd.begin(); it != vd.end() && sigo; ++it){ //Se busca si es no nulo
 		if(*it.first == c){
 			referencia = it;
 			sigo = false;
 		}
 	}
-	
+
 	if (sigo) referencia = &v_nulo;				//Si es nulo se devuelve una referencia a el atributo de la clase,
 												//Por qué? no se
-	
+
 	return referencia;
+}
+
+template <class T>
+typename vectorD<T>::bool vectorD<T>::operator==(const vectorD<T> & x)
+{
+	bool iguales = false;
+
+    if(n_ele == x.n_ele){
+        if(v_nulo == x.v_nulo){
+            if(vd.size() == x.vd.size()){
+
+                iguales = *this == x;
+            }
+        }
+    }
+
+	return iguales;
+}
+
+template <class T>
+typename vectorD<T>::bool vectorD<T>::operator!=(const vectorD<T> & x)
+{
+	return !(*this == x);
 }
